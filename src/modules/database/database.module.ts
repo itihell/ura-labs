911b2842b3790c76b70176b765069d4fbdd56bd1
Itihell Mejia <itihell.mejia@gmail.com>
2023-06-28 19:31:48 -0600

    Instalando typeorme y configuracion a la BD

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { Database } from './database';
 
-@Module({})
+@Module({
+  imports: [Database],
+  exports: [Database],
+})
 export class DatabaseModule {}
