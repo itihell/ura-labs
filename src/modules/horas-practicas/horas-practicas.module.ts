6951953a9799f41562abe63cb01e52d57ffd25a4
jasson <jeicobfer@gmail.com>
2023-08-19 20:08:58 -0600

    cambiando archivos de mi modulo

--- src/modules/horas-practicas/horas-practicas.module.ts
+++ src/modules/horas-practicas/horas-practicas.module.ts
@@ -4,10 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HorasPracticasController } from './controllers/horas-practicas.controller';
 import { HorasPracticasService } from './services/horas-practicas.service';
 import { HorasPracticas } from './entities/horas-practicas.entity';
-import { Carreras } from './entities/carreras.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HorasPracticas, Carreras])],
+  imports: [TypeOrmModule.forFeature([HorasPracticas])],
   controllers: [HorasPracticasController],
   providers: [HorasPracticasService],
 })
