28eadd941d73db6227fcbca640655e8eb0fa9f72
TaniaCruz09 <105082338+TaniaCruz09@users.noreply.github.com>
2023-08-27 19:34:28 -0600

    Update registro-carreras.module.ts

--- src/modules/registro-carreras/registro-carreras.module.ts
+++ src/modules/registro-carreras/registro-carreras.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Area, Carrera } from './entities';
 import { RegistroCarrerasController } from './controllers/registro-carrera.controller';
+import { RegistroAreaController } from './controllers/registro-area.controller';
 import { RegistroCarrerasService } from './services/registro-carrera.service';
+import { RegistroAreaService } from './services/registro-areas.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Area, Carrera])],
-    controllers: [RegistroCarrerasController, RegistroAreaController],
+  controllers: [RegistroCarrerasController, RegistroAreaController],
   providers: [RegistroCarrerasService, RegistroAreaService],
 })
 export class RegistroCarrerasModule {}
