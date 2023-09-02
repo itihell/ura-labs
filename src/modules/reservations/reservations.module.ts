dbbd0830a0e841765d87bd20e0f6ea78d43d41f0
Arlen Assael Marenco Jaime <arlen.marenco.8uoj@est.uraccan.edu.ni>
2023-09-02 17:37:14 -0600

    Upload Reworked Code All Working correctly
    -everything changed
    -need to add some fields to the table

--- src/modules/reservations/reservations.module.ts
+++ src/modules/reservations/reservations.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ReservationsService } from './reservations.service';
-import { ReservationsController } from './reservations.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
 import { Reservation } from './entities/reservation.entity';
+import { LabRegisterModule } from '../lab-register/lab.module';
+import { LabEntity } from '../lab-register/entities';
+import { LabRegisterService } from '../lab-register/services';
+import { UsersService } from '../auth/services';
+import { User } from '../auth/entities';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation])],
+  imports: [
+    TypeOrmModule.forFeature([Reservation, LabEntity, User]),
+    LabRegisterModule,
+    AuthModule, // Importa el módulo de registro de laboratorio para usarlo en el servicio
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, LabRegisterService, UsersService],
 })
 export class ReservationsModule {}
--- src/modules/reservations/validators/time-order.validator.ts
+++ src/modules/reservations/validators/time-order.validator.ts
@@ -4,28 +4,29 @@ import {
   ValidationArguments,
 } from 'class-validator';
 
-@ValidatorConstraint({ name: 'timeOrderAndFormat', async: false })
-export class TimeOrderAndFormatValidator
-  implements ValidatorConstraintInterface
-{
+@ValidatorConstraint({ name: 'timeOrder', async: false })
+export class TimeOrderValidator implements ValidatorConstraintInterface {
   validate(value: any, args: ValidationArguments) {
     const startTime = args.object[args.constraints[0]];
     const endTime = value;
 
-    if (!startTime || !endTime) {
-      return true; // Si alguna de las horas no está definida, la validación no es aplicable
+    // Verifica que startTime y endTime estén definidos y sean cadenas de caracteres
+    if (typeof startTime !== 'string' || typeof endTime !== 'string') {
+      return false;
     }
 
-    const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+    // Verifica que startTime y endTime tengan el formato "HH:MM"
+    if (
+      !/^([01]\d|2[0-3]):([0-5]\d)$/.test(startTime) ||
+      !/^([01]\d|2[0-3]):([0-5]\d)$/.test(endTime)
+    ) {
+      return false;
+    }
 
-    return (
-      timePattern.test(startTime) &&
-      timePattern.test(endTime) &&
-      startTime < endTime
-    );
+    return startTime < endTime;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return 'La hora de inicio y fin deben estar en formato HH:MM, y la hora de inicio debe ser anterior a la hora de fin.';
+    return 'La hora de inicio debe ser anterior a la hora de fin.';
   }
 }
