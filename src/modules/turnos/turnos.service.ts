a96bffb0bfb73b7a36af68fccbca91f56a4676ec
Arlen Assael Marenco Jaime <91499118+NightCW34@users.noreply.github.com>
2023-11-21 14:57:20 -0600

    Add all Https Request to Turnos

--- src/modules/turnos/turnos.service.ts
+++ src/modules/turnos/turnos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Turnos } from './entities/turnos.entity';
@@ -15,10 +15,32 @@ export class TurnosService {
   }
 
   async findOne(id: number): Promise<Turnos> {
-    return await this.turnosRepository.findOne({ where: { id } });
+    const turnos = await this.turnosRepository.findOne({ where: { id } });
+    if (!turnos) {
+      throw new NotFoundException(`Turno con ID ${id} no encontrado`);
+    }
+    return turnos;
   }
 
   async findAll(): Promise<Turnos[]> {
     return await this.turnosRepository.find();
+  }
+
+  async update(id: number, updateTurnosDto: Partial<Turnos>): Promise<Turnos> {
+    const turnos = await this.turnosRepository.preload({
+      id,
+      ...updateTurnosDto,
+    });
+    if (!turnos) {
+      throw new NotFoundException(`Turno con ID ${id} no encontrado`);
+    }
+    return await this.turnosRepository.save(turnos);
+  }
+
+  async delete(id: number): Promise<void> {
+    const result = await this.turnosRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Turno con ID ${id} no encontrado`);
+    }
   }
 }
